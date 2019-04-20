391f6b691068cd38ff631a21dd618a8f263e0e1b
oneilld136 <oneilld136@gmail.com>
2019-04-20 19:53:12 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+#toppings = ["pickles", "mushrooms", "bacon"]
+def my_each(array)
+  counter = 0
+  while counter < array.length
+    yield(array[counter])
+    counter = counter +1
+  end
+  array
 end
